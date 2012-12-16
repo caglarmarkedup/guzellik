2313e2ccdbc0a8fca737a417815e3c195d635283
Caglar Oner <caglaroner@hotmail.com>
2012-12-15 20:20:24 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guzellik::Application.config.secret_token = '40df0b62284c1a116afd452c0f95d79f9a6eecf4342541cfe19bea55fbf6b0945a6edaec9551a05bca36d15248c97c9cd59dcd3180249b04f4c69c0ce83632ce'
